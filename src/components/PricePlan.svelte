e324763bcaf95cea43f8f5da92fc7800d41d058c
Sergey <s3gaddtv@gmail.com>
2020-09-15 18:11:25 +0300

    style: add active-class for price plan

--- src/components/PricePlan.svelte
+++ src/components/PricePlan.svelte
@@ -1,29 +1,40 @@
 <script>
+  import { DEFAULT_PLAN_CLASS, SELECTED_ELEMENT_CLASS } from '../constants/className';
+  
   export let pricePlansInfo;
   export let setNextStep;
   export let setFieldInfo;
-  let userPlan;
+
+  let planDivClassName = new Array(pricePlansInfo.length).fill(DEFAULT_PLAN_CLASS);
+  let choosenPlan;
+  let choosenPlanID = 0;
+
+  $: planDivClassName = planDivClassName.map((className, id) => {
+    if (choosenPlanID === id) {
+      return `${className} ${SELECTED_ELEMENT_CLASS}`;
+    };
+
+    return DEFAULT_PLAN_CLASS;
+  });
 
   const choosePricePlan = ({target}) => {
     const choosenPlanDiv = target.closest('.group__item');
 
     if (choosenPlanDiv) {
-      userPlan = choosenPlanDiv.dataset.name;
+      choosenPlan = choosenPlanDiv.dataset.name;
 
-      setFieldInfo({userPlan});
-
-      document.querySelectorAll('.group__item').forEach((item) => {
-        item.classList.remove('item--selected');
-        
-        if (item.dataset.name === userPlan) {
-          item.classList.add('item--selected');
+      document.querySelectorAll('.group__item').forEach((item, id) => {
+       
+        if (item.dataset.name === choosenPlan) {
+          choosenPlanID = id;
         }
       });
     }
   };
 
   const switchStep = () => {
-    if (userPlan) {
+    if (choosenPlan) {  
+      setFieldInfo({choosenPlan});
       setNextStep();
     }
   };
@@ -31,8 +42,15 @@
 
 <div class="sign-up">
   <div class="plan-group" on:click={choosePricePlan}>
-    {#each pricePlansInfo as {name, description, price} (name)}
-      <div class="group__item" data-name={name}>
+    <div class="history-url">
+      <span class="url__link link--active">Choose Plan</span>
+      <span class="url__pointer">&gt;</span>
+      <span class="url__link">Create account</span>
+      <span class="url__pointer">&gt;</span>
+      <span class="url__link">Complete order</span>
+    </div>
+    {#each pricePlansInfo as {name, description, price}, id (name)}
+      <div class={planDivClassName[id]} data-name={name}>
         <div class="item__info">
           <h3>{name}</h3>
           <p>{description}</p>
@@ -53,7 +71,6 @@
 
   .plan-group {
     margin: 0 auto;
-    margin-top: 151px; 
     display: flex;
     flex-direction: column;
     align-items: flex-end;
@@ -63,8 +80,8 @@
 
   .group__item {
     position: relative;
-    padding: 1.3em 3% 1.3em 10%;
     margin-bottom: 0.5em;
+    padding: 1.3em 3% 1.3em 10%;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -75,15 +92,31 @@
     cursor: pointer;
   }
 
-  .group__item::before {
+  .group__item::before,
+  .item--selected::after {
     position: absolute;
     top: calc(50% - 6px);
     left: 3%;
     width: 12px;
     height: 12px;
-    border: 2px solid #000;
     border-radius: 50%;
     content: '';
+  }
+
+  .group__item::before {
+    border: 2px solid #000;
+  }
+
+  .item--selected::before {
+    border-color: #fede00;
+  }
+
+  .item--selected::after {
+    top: calc(50% - 1px);
+    left: calc(3% + 5px);
+    width: 6px;
+    height: 6px;
+    background: #000;
   }
 
   .group__item:hover {
@@ -91,9 +124,37 @@
   }
 
   .next-step-btn {
+    margin-top: 2em;
     width: 135px;
     height: 50px;
     background-color: #359be5;
     color: #fff;
+  }
+
+  .history-url {
+    margin: 3em 0;
+    width: 100%;
+    align-self: flex-start;
+    font-size: 1.2rem;
+    text-align: justify;
+  }
+
+  .url__link, 
+  .url__pointer {
+    cursor: default;
+    color: #808080;
+  }
+
+  .url__pointer {
+    padding: 0 5%;
+  }
+
+  .link--active {
+    color: #000;
+    cursor: pointer;
+  }
+
+  h2, h3 {
+    font-weight: 600;
   }
 </style>
---
+++ src/constants/className.js
@@ -0,0 +1,2 @@
+export const DEFAULT_PLAN_CLASS = 'group__item';
+export const SELECTED_ELEMENT_CLASS = 'item--selected';
